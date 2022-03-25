288bd4eada0178ae34e8482a6388a8648df7b41b
Tyler Savery <tyler@theyoungastronauts.com>
2022-03-25 10:33:41 -0400

    send form with lovely paste tools

--- lib/features/send/components/send_form.dart
+++ lib/features/send/components/send_form.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,6 +9,7 @@ import 'package:rbx_wallet/core/components/buttons.dart';
 import 'package:rbx_wallet/core/theme/app_theme.dart';
 import 'package:rbx_wallet/features/send/providers/send_form_provider.dart';
 import 'package:rbx_wallet/features/wallet/models/wallet.dart';
+import 'package:rbx_wallet/utils/toast.dart';
 
 class SendForm extends BaseComponent {
   final Wallet wallet;
@@ -14,12 +17,30 @@ class SendForm extends BaseComponent {
 
   final GlobalKey<FormState> _formKey = GlobalKey();
 
+  Future<void> _pasteAddress(SendFormProvider formProvider) async {
+    ClipboardData? clipboardData =
+        await Clipboard.getData(Clipboard.kTextPlain);
+    if (clipboardData != null && clipboardData.text != null) {
+      final normalizedText =
+          clipboardData.text!.replaceAll(RegExp('[^a-zA-Z0-9]'), "");
+      formProvider.addressController.text = normalizedText;
+    } else {
+      Toast.error("Clipboard text is invalid");
+    }
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     const leadingWidth = 60.0;
 
     final formProvider = ref.read(sendFormProvider.notifier);
     final formModel = ref.watch(sendFormProvider);
+
+    String pasteMessage = "Use ctrl+v to paste or click ";
+
+    if (Platform.isMacOS) {
+      pasteMessage = pasteMessage.replaceAll("ctrl+v", "cmd+v");
+    }
 
     return Form(
       key: _formKey,
@@ -48,6 +69,34 @@ class SendForm extends BaseComponent {
                   inputFormatters: [
                     FilteringTextInputFormatter.allow(RegExp('[a-zA-Z0-9]')),
                   ],
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 3),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(pasteMessage),
+                      InkWell(
+                        onTap: () {
+                          _pasteAddress(formProvider);
+                        },
+                        child: Text(
+                          "here",
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                      ),
+                      Text("."),
+                    ],
+                  ),
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.paste),
+                  onPressed: () {
+                    _pasteAddress(formProvider);
+                  },
                 ),
               ),
               ListTile(
