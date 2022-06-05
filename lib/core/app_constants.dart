f809656ad3f74d619841efc84b1d4de52ec2ab2e
Tyler Savery <tyler@theyoungastronauts.com>
2022-06-05 19:03:16 -0400

    version incr

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
-const APP_VERSION = "pre21.5";
+const APP_VERSION = "pre21.6";
 const REFRESH_TIMEOUT_SECONDS = 10;
 const ALLOW_DOUBLE_MINTES = false;
 const MIN_RBX_FOR_SC_ACTION = 0.1;
