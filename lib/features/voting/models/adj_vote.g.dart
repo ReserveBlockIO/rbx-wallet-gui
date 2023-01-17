ae69efcbc5db47b912e3fe0d6a326df2e2eafbb9
Manuel Chalá <manuel@theyoungastronauts.com>
2023-01-17 12:12:52 -0500

    Added the details of the adjudicator in vote to the vote detail screen

--- lib/features/voting/models/adj_vote.g.dart
+++ lib/features/voting/models/adj_vote.g.dart
@@ -9,16 +9,16 @@ part of 'adj_vote.dart';
 _$_AdjVote _$$_AdjVoteFromJson(Map<String, dynamic> json) => _$_AdjVote(
       rbxAddress: json['RBXAddress'] as String,
       ipAddress: json['IPAddress'] as String,
-      provider: $enumDecode(_$ProviderEnumMap, json['ProviderForMachine']),
+      provider: providerFromJson(json['ProviderForMachine'] as int),
       machineType: json['MachineType'] as String,
-      machineOs: $enumDecode(_$OSEnumMap, json['MachineOS']),
+      machineOs: osFromJson(json['MachineOS'] as int),
       machineRam: json['MachineRam'] as int,
       machineCPU: json['MachineCPU'] as String,
       machineCPUCores: json['MachineCPUCores'] as int,
       machineCPUThreads: json['MachineCPUThreads'] as int,
       machineHDDSize: json['MachineHDDSize'] as int,
       machineHDDSpecifier:
-          $enumDecode(_$HDSizeSpecifierEnumMap, json['MachineHDDSpecifier']),
+          hdSizeSpecifierFromJson(json['MachineHDDSpecifier'] as int),
       internetSpeedUp: json['InternetSpeedUp'] as int,
       internetSpeedDown: json['InternetSpeedDown'] as int,
       bandwith: json['Bandwith'] as int,
@@ -50,23 +50,3 @@ Map<String, dynamic> _$$_AdjVoteToJson(_$_AdjVote instance) =>
       'GithubLink': instance.githubLink,
       'SupplementalURLs': instance.supplementalURLs,
     };
-
-const _$ProviderEnumMap = {
-  Provider.onlineCloudVPS: 'onlineCloudVPS',
-  Provider.onlineDedicated: 'onlineDedicated',
-  Provider.localDedicated: 'localDedicated',
-  Provider.homeMachine: 'homeMachine',
-  Provider.officeMachine: 'officeMachine',
-};
-
-const _$OSEnumMap = {
-  OS.linux: 'linux',
-  OS.windows: 'windows',
-  OS.mac: 'mac',
-};
-
-const _$HDSizeSpecifierEnumMap = {
-  HDSizeSpecifier.gb: 'gb',
-  HDSizeSpecifier.tb: 'tb',
-  HDSizeSpecifier.pb: 'pb',
-};
