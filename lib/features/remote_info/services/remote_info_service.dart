364de699894439963321e2ac77da7205cb312d7e
Tyler Savery <tyler@theyoungastronauts.com>
2023-02-12 13:08:13 -0500

    debug logging started

--- lib/features/remote_info/services/remote_info_service.dart
+++ lib/features/remote_info/services/remote_info_service.dart
@@ -1,15 +1,35 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:rbx_wallet/core/env.dart';
 import 'package:rbx_wallet/features/remote_info/models/remote_info.dart';
+import 'package:rbx_wallet/utils/files.dart';
 
 class RemoteInfoService {
   static Future<RemoteInfo?> fetchInfo() async {
     try {
       final response = await Dio(BaseOptions(baseUrl: Env.explorerApiBaseUrl)).get('/applications/');
-      print(response);
       return RemoteInfo.fromJson(response.data);
-    } catch (e) {
+    } catch (e, st) {
       print(e);
+
+      final path = await dbPath();
+      final debugOutputPath = "$path/Databases/debug-gui.txt";
+      print(debugOutputPath);
+
+      if (!File(debugOutputPath).existsSync()) {
+        File(debugOutputPath).createSync();
+      }
+
+      final currentLines = await File(debugOutputPath).readAsLines();
+
+      final newLines = [e.toString(), st.toString()];
+
+      final linesToWrite = [...currentLines, "", "----", "", ...newLines];
+      print(debugOutputPath);
+
+      File(debugOutputPath).writeAsStringSync(linesToWrite.join("\n"));
+
       return null;
     }
   }
