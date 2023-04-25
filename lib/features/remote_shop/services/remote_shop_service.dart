8b4fd58ba2c6580b6af8bf45ef240d670053309e
Tyler Savery <tyler@theyoungastronauts.com>
2023-04-25 11:42:39 -0400

    integrated GetDecShopStateTreiList endpoint

--- lib/features/remote_shop/services/remote_shop_service.dart
+++ lib/features/remote_shop/services/remote_shop_service.dart
@@ -175,12 +175,14 @@ class RemoteShopService extends BaseService {
   // }
 
   Future<List<DecShop>> listRemoteShops() async {
-    const tempDataStr =
-        '[{"Id":34,"UniqueId":"KsQmYokSEJ1681996900","Name":"RBX Testnet Shop","DecShopURL":"rbx://rbxtestnetshop","ThirdPartyBaseURL":null,"ThirdPartyAPIURL":null,"Description":"My new shop.","OwnerAddress":"xNsQq9C7mXYGV7VFG17QZx1WZLP6BbnR7d","HostingType":0,"IP":"51.222.206.200","Port":13341,"STUNServerGroup":0,"OriginalBlockHeight":85405,"OriginalTXHash":"d571f724be511d0be6e26c8c78f870db3bbd59c73e57ae83ef2a2dc6e84fba20","LatestBlockHeight":85405,"LatestTXHash":"d571f724be511d0be6e26c8c78f870db3bbd59c73e57ae83ef2a2dc6e84fba20","UpdateTimestamp":0,"AutoUpdateNetworkDNS":true,"NeedsPublishToNetwork":false,"IsOffline":false,"IsPublished":true,"CollectionCount":0,"ListingCount":0,"AuctionCount":0,"IsIPDifferent":true}, {"Id":38,"UniqueId":"IlftxfSNlq1682384797","Name":"Presidents","DecShopURL":"rbx://Prez","ThirdPartyBaseURL":null,"ThirdPartyAPIURL":null,"Description":"Presidents","OwnerAddress":"xWURceUzoxsmTgKkRjGDFeJPhraEmsBVfD","HostingType":0,"IP":"75.41.218.48","Port":13341,"STUNServerGroup":1,"OriginalBlockHeight":104183,"OriginalTXHash":"5af6e1384c9cfdc960b04b71bfc4e9293f3dcb6fabbf982e1db146432e9983de","LatestBlockHeight":104183,"LatestTXHash":"5af6e1384c9cfdc960b04b71bfc4e9293f3dcb6fabbf982e1db146432e9983de","UpdateTimestamp":0,"AutoUpdateNetworkDNS":true,"NeedsPublishToNetwork":false,"IsOffline":false,"IsPublished":true,"CollectionCount":0,"ListingCount":0,"AuctionCount":0,"IsIPDifferent":true},{"Id":39,"UniqueId":"WZUXfFjHEp1682386473","Name":"city","DecShopURL":"rbx://city","ThirdPartyBaseURL":null,"ThirdPartyAPIURL":null,"Description":"city","OwnerAddress":"xUHfmVW4PgFGsQMqJunWRpgcJ1Y7Md3bqF","HostingType":0,"IP":"31.220.93.82","Port":13341,"STUNServerGroup":1,"OriginalBlockHeight":104262,"OriginalTXHash":"29b58f9d695d843d082a712277b02b64edb4e8527c7bc8a1c829d0d7941146bb","LatestBlockHeight":104262,"LatestTXHash":"29b58f9d695d843d082a712277b02b64edb4e8527c7bc8a1c829d0d7941146bb","UpdateTimestamp":0,"AutoUpdateNetworkDNS":true,"NeedsPublishToNetwork":false,"IsOffline":false,"IsPublished":true,"CollectionCount":0,"ListingCount":0,"AuctionCount":0,"IsIPDifferent":true}]';
+    final response = await getJson("/GetDecShopStateTreiList", cleanPath: false);
 
-    await Future.delayed(Duration(milliseconds: 500));
-    final List<dynamic> tempData = jsonDecode(tempDataStr);
+    if (response['Success'] == true) {
+      final List<dynamic> items = response['DecShops'];
+      return items.map((item) => DecShop.fromJson(item)).toList();
+    }
 
-    return tempData.map((item) => DecShop.fromJson(item)).toList();
+    Toast.error(response['Message'] ?? "A problem occurred");
+    return [];
   }
 }
