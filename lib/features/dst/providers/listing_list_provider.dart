0250825271c41d1d98e55fb5d29441a898c26eea
Tyler Savery <tyler@theyoungastronauts.com>
2023-04-25 20:58:59 -0400

    update

--- lib/features/dst/providers/listing_list_provider.dart
+++ lib/features/dst/providers/listing_list_provider.dart
@@ -6,22 +6,23 @@ import '../models/listing.dart';
 
 class ListingListProvider extends StateNotifier<List<Listing>> {
   final Ref ref;
-  final storeId;
+  final collectionId;
 
-  ListingListProvider(this.ref, this.storeId, [List<Listing> listings = const []]) : super(listings) {
-    load(storeId);
+  ListingListProvider(this.ref, this.collectionId, [List<Listing> listings = const []]) : super(listings) {
+    load(collectionId);
   }
 
-  Future<void> load(int storeId) async {
-    state = await DstService().listListings(storeId);
+  Future<void> load(int collectionId) async {
+    print("Getting collections");
+    state = await DstService().listListings(collectionId);
   }
 
   void refresh() {
-    load(storeId);
+    load(collectionId);
     ref.read(listedNftsProvider.notifier).refresh();
   }
 }
 
 final listingListProvider = StateNotifierProvider.family<ListingListProvider, List<Listing>, int>(
-  (ref, storeId) => ListingListProvider(ref, storeId),
+  (ref, collectionId) => ListingListProvider(ref, collectionId),
 );
