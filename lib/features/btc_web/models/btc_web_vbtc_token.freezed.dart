3820480c4df47a224ae35b3442d4d12b6b42e0f9
Manuel Chalá <manuel@theyoungastronauts.com>
2024-10-08 09:57:28 -0500

    Added the btc web vbtc token model

---
+++ lib/features/btc_web/models/btc_web_vbtc_token.freezed.dart
@@ -0,0 +1,363 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'btc_web_vbtc_token.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+BtcWebVbtcToken _$BtcWebVbtcTokenFromJson(Map<String, dynamic> json) {
+  return _BtcWebVbtcToken.fromJson(json);
+}
+
+/// @nodoc
+mixin _$BtcWebVbtcToken {
+  String get name => throw _privateConstructorUsedError;
+  String get descripton => throw _privateConstructorUsedError;
+  @JsonKey(name: 'sc_identifier')
+  String get scIdentifier => throw _privateConstructorUsedError;
+  @JsonKey(name: 'owner_address')
+  String get ownerAddress => throw _privateConstructorUsedError;
+  @JsonKey(name: 'image_url')
+  String get imageUrl => throw _privateConstructorUsedError;
+  @JsonKey(name: 'deposit_address')
+  String get depositAddress => throw _privateConstructorUsedError;
+  @JsonKey(name: 'public_key_proofs')
+  String get publicKeyProofs => throw _privateConstructorUsedError;
+  @JsonKey(name: 'global_balance')
+  double get globalBalance => throw _privateConstructorUsedError;
+  @JsonKey(name: 'created_at')
+  DateTime get createdAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $BtcWebVbtcTokenCopyWith<BtcWebVbtcToken> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BtcWebVbtcTokenCopyWith<$Res> {
+  factory $BtcWebVbtcTokenCopyWith(
+          BtcWebVbtcToken value, $Res Function(BtcWebVbtcToken) then) =
+      _$BtcWebVbtcTokenCopyWithImpl<$Res, BtcWebVbtcToken>;
+  @useResult
+  $Res call(
+      {String name,
+      String descripton,
+      @JsonKey(name: 'sc_identifier') String scIdentifier,
+      @JsonKey(name: 'owner_address') String ownerAddress,
+      @JsonKey(name: 'image_url') String imageUrl,
+      @JsonKey(name: 'deposit_address') String depositAddress,
+      @JsonKey(name: 'public_key_proofs') String publicKeyProofs,
+      @JsonKey(name: 'global_balance') double globalBalance,
+      @JsonKey(name: 'created_at') DateTime createdAt});
+}
+
+/// @nodoc
+class _$BtcWebVbtcTokenCopyWithImpl<$Res, $Val extends BtcWebVbtcToken>
+    implements $BtcWebVbtcTokenCopyWith<$Res> {
+  _$BtcWebVbtcTokenCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? descripton = null,
+    Object? scIdentifier = null,
+    Object? ownerAddress = null,
+    Object? imageUrl = null,
+    Object? depositAddress = null,
+    Object? publicKeyProofs = null,
+    Object? globalBalance = null,
+    Object? createdAt = null,
+  }) {
+    return _then(_value.copyWith(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      descripton: null == descripton
+          ? _value.descripton
+          : descripton // ignore: cast_nullable_to_non_nullable
+              as String,
+      scIdentifier: null == scIdentifier
+          ? _value.scIdentifier
+          : scIdentifier // ignore: cast_nullable_to_non_nullable
+              as String,
+      ownerAddress: null == ownerAddress
+          ? _value.ownerAddress
+          : ownerAddress // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      depositAddress: null == depositAddress
+          ? _value.depositAddress
+          : depositAddress // ignore: cast_nullable_to_non_nullable
+              as String,
+      publicKeyProofs: null == publicKeyProofs
+          ? _value.publicKeyProofs
+          : publicKeyProofs // ignore: cast_nullable_to_non_nullable
+              as String,
+      globalBalance: null == globalBalance
+          ? _value.globalBalance
+          : globalBalance // ignore: cast_nullable_to_non_nullable
+              as double,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_BtcWebVbtcTokenCopyWith<$Res>
+    implements $BtcWebVbtcTokenCopyWith<$Res> {
+  factory _$$_BtcWebVbtcTokenCopyWith(
+          _$_BtcWebVbtcToken value, $Res Function(_$_BtcWebVbtcToken) then) =
+      __$$_BtcWebVbtcTokenCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String name,
+      String descripton,
+      @JsonKey(name: 'sc_identifier') String scIdentifier,
+      @JsonKey(name: 'owner_address') String ownerAddress,
+      @JsonKey(name: 'image_url') String imageUrl,
+      @JsonKey(name: 'deposit_address') String depositAddress,
+      @JsonKey(name: 'public_key_proofs') String publicKeyProofs,
+      @JsonKey(name: 'global_balance') double globalBalance,
+      @JsonKey(name: 'created_at') DateTime createdAt});
+}
+
+/// @nodoc
+class __$$_BtcWebVbtcTokenCopyWithImpl<$Res>
+    extends _$BtcWebVbtcTokenCopyWithImpl<$Res, _$_BtcWebVbtcToken>
+    implements _$$_BtcWebVbtcTokenCopyWith<$Res> {
+  __$$_BtcWebVbtcTokenCopyWithImpl(
+      _$_BtcWebVbtcToken _value, $Res Function(_$_BtcWebVbtcToken) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? descripton = null,
+    Object? scIdentifier = null,
+    Object? ownerAddress = null,
+    Object? imageUrl = null,
+    Object? depositAddress = null,
+    Object? publicKeyProofs = null,
+    Object? globalBalance = null,
+    Object? createdAt = null,
+  }) {
+    return _then(_$_BtcWebVbtcToken(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      descripton: null == descripton
+          ? _value.descripton
+          : descripton // ignore: cast_nullable_to_non_nullable
+              as String,
+      scIdentifier: null == scIdentifier
+          ? _value.scIdentifier
+          : scIdentifier // ignore: cast_nullable_to_non_nullable
+              as String,
+      ownerAddress: null == ownerAddress
+          ? _value.ownerAddress
+          : ownerAddress // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      depositAddress: null == depositAddress
+          ? _value.depositAddress
+          : depositAddress // ignore: cast_nullable_to_non_nullable
+              as String,
+      publicKeyProofs: null == publicKeyProofs
+          ? _value.publicKeyProofs
+          : publicKeyProofs // ignore: cast_nullable_to_non_nullable
+              as String,
+      globalBalance: null == globalBalance
+          ? _value.globalBalance
+          : globalBalance // ignore: cast_nullable_to_non_nullable
+              as double,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_BtcWebVbtcToken extends _BtcWebVbtcToken {
+  _$_BtcWebVbtcToken(
+      {required this.name,
+      required this.descripton,
+      @JsonKey(name: 'sc_identifier') required this.scIdentifier,
+      @JsonKey(name: 'owner_address') required this.ownerAddress,
+      @JsonKey(name: 'image_url') required this.imageUrl,
+      @JsonKey(name: 'deposit_address') required this.depositAddress,
+      @JsonKey(name: 'public_key_proofs') required this.publicKeyProofs,
+      @JsonKey(name: 'global_balance') required this.globalBalance,
+      @JsonKey(name: 'created_at') required this.createdAt})
+      : super._();
+
+  factory _$_BtcWebVbtcToken.fromJson(Map<String, dynamic> json) =>
+      _$$_BtcWebVbtcTokenFromJson(json);
+
+  @override
+  final String name;
+  @override
+  final String descripton;
+  @override
+  @JsonKey(name: 'sc_identifier')
+  final String scIdentifier;
+  @override
+  @JsonKey(name: 'owner_address')
+  final String ownerAddress;
+  @override
+  @JsonKey(name: 'image_url')
+  final String imageUrl;
+  @override
+  @JsonKey(name: 'deposit_address')
+  final String depositAddress;
+  @override
+  @JsonKey(name: 'public_key_proofs')
+  final String publicKeyProofs;
+  @override
+  @JsonKey(name: 'global_balance')
+  final double globalBalance;
+  @override
+  @JsonKey(name: 'created_at')
+  final DateTime createdAt;
+
+  @override
+  String toString() {
+    return 'BtcWebVbtcToken(name: $name, descripton: $descripton, scIdentifier: $scIdentifier, ownerAddress: $ownerAddress, imageUrl: $imageUrl, depositAddress: $depositAddress, publicKeyProofs: $publicKeyProofs, globalBalance: $globalBalance, createdAt: $createdAt)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_BtcWebVbtcToken &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.descripton, descripton) ||
+                other.descripton == descripton) &&
+            (identical(other.scIdentifier, scIdentifier) ||
+                other.scIdentifier == scIdentifier) &&
+            (identical(other.ownerAddress, ownerAddress) ||
+                other.ownerAddress == ownerAddress) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            (identical(other.depositAddress, depositAddress) ||
+                other.depositAddress == depositAddress) &&
+            (identical(other.publicKeyProofs, publicKeyProofs) ||
+                other.publicKeyProofs == publicKeyProofs) &&
+            (identical(other.globalBalance, globalBalance) ||
+                other.globalBalance == globalBalance) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      name,
+      descripton,
+      scIdentifier,
+      ownerAddress,
+      imageUrl,
+      depositAddress,
+      publicKeyProofs,
+      globalBalance,
+      createdAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_BtcWebVbtcTokenCopyWith<_$_BtcWebVbtcToken> get copyWith =>
+      __$$_BtcWebVbtcTokenCopyWithImpl<_$_BtcWebVbtcToken>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_BtcWebVbtcTokenToJson(
+      this,
+    );
+  }
+}
+
+abstract class _BtcWebVbtcToken extends BtcWebVbtcToken {
+  factory _BtcWebVbtcToken(
+      {required final String name,
+      required final String descripton,
+      @JsonKey(name: 'sc_identifier')
+          required final String scIdentifier,
+      @JsonKey(name: 'owner_address')
+          required final String ownerAddress,
+      @JsonKey(name: 'image_url')
+          required final String imageUrl,
+      @JsonKey(name: 'deposit_address')
+          required final String depositAddress,
+      @JsonKey(name: 'public_key_proofs')
+          required final String publicKeyProofs,
+      @JsonKey(name: 'global_balance')
+          required final double globalBalance,
+      @JsonKey(name: 'created_at')
+          required final DateTime createdAt}) = _$_BtcWebVbtcToken;
+  _BtcWebVbtcToken._() : super._();
+
+  factory _BtcWebVbtcToken.fromJson(Map<String, dynamic> json) =
+      _$_BtcWebVbtcToken.fromJson;
+
+  @override
+  String get name;
+  @override
+  String get descripton;
+  @override
+  @JsonKey(name: 'sc_identifier')
+  String get scIdentifier;
+  @override
+  @JsonKey(name: 'owner_address')
+  String get ownerAddress;
+  @override
+  @JsonKey(name: 'image_url')
+  String get imageUrl;
+  @override
+  @JsonKey(name: 'deposit_address')
+  String get depositAddress;
+  @override
+  @JsonKey(name: 'public_key_proofs')
+  String get publicKeyProofs;
+  @override
+  @JsonKey(name: 'global_balance')
+  double get globalBalance;
+  @override
+  @JsonKey(name: 'created_at')
+  DateTime get createdAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$_BtcWebVbtcTokenCopyWith<_$_BtcWebVbtcToken> get copyWith =>
+      throw _privateConstructorUsedError;
+}
