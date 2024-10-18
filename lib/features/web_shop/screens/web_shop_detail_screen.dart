8e6c5036584040970d169d0e20bce838c5dc2085
Manuel Chalá <manuel@theyoungastronauts.com>
2024-10-18 14:49:44 -0500

    UI/UX fixes to the bottom action buttos in the dec shop forms and detail screens

--- lib/features/web_shop/screens/web_shop_detail_screen.dart
+++ lib/features/web_shop/screens/web_shop_detail_screen.dart
@@ -218,7 +218,12 @@ class WebShopDetailScreen extends BaseScreen {
                   Container(
                     color: Colors.black,
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.only(
+                        left: 8.0,
+                        top: 16.0,
+                        right: 8.0,
+                        bottom: 48.0,
+                      ),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
